c4060b63f55a3b9e2979980a5d8a583ef1b67b1d
mtoto <tszilagyi@outlook.com>
2018-07-04 18:13:41 +0200

    static workload example

---
+++ static/monte-carlo.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+n <- as.double(args[1])
+
+mean_change = 1.001 
+volatility = 0.01 
+opening_price = 100 
+
+getClosingPrice <- function(days) { 
+        movement <- rnorm(days, mean=mean_change, sd=volatility) 
+        path <- cumprod(c(opening_price, movement)) 
+        closingPrice <- path[days] 
+        return(closingPrice) 
+} 
+
+replicate(100000, getClosingPrice(365)) 
