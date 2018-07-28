ab687057f1ba9c9a46efe0f0dc9467a6f38c8a9d
mtoto <tszilagyi@outlook.com>
2018-07-28 11:29:49 +0200

    work work work work work

---
+++ fine/functions.R
@@ -0,0 +1,21 @@
+# modeling function
+run_save_model <- function(method) {
+        library(mlbench)
+        library(caret)
+        data("BostonHousing")
+        # split data
+        set.seed(123)
+        train_index <- createDataPartition(BostonHousing$medv,1, p = .7)
+        train <- BostonHousing[train_index[[1]],]
+        # train model
+        model <- train(medv ~., 
+                       data = train, 
+                       method = method)
+        
+        # upload to storage bucket
+        file <- sprintf("%s_model.rds", method)
+        saveRDS(model, file)
+        gcs_upload(file, 
+                   name = file,
+                   bucket = "bostonmodels")
+}
