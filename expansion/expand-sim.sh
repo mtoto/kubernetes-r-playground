37761e77e945fcc204d1fb94fc328637af616753
mtoto <tszilagyi@outlook.com>
2018-08-06 16:59:16 +0200

    delete gibberish

--- expansion/expand-sim.sh
+++ expansion/expand-sim.sh
@@ -4,10 +4,3 @@ for i in {1..100}
 do
   cat job.yaml | sed "s/\$ITEM/$i/" > ./jobs/job-$i.yaml
 done
-
-set -x
-
-for j in $(kubectl get jobs -o custom-columns=:.metadata.name)
-do
-    kubectl delete jobs $j &
-done
